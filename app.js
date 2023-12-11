4a1f33364affae13e4c3e724c997091b91cb6abd
elouand <Elouandgamer@hotmail.com>
2023-12-11 13:25:19 +0100

    app.js rempli

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { isNumberObject } = require('util/types');
+const server = http.createServer(app);
+const io = new require("socket.io")(server);
+
+app.use(express.static(__dirname));
+
+const PORT = process.env.PORT || 8888;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+//-------------------------------Express-------------------------------------------
+app.get('/', (request, response) => {
+  response.sendFile('index.html', {root: __dirname});
+});
