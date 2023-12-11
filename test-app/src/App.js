6f6dbbe86b41d96c5fa5fb9de4091325bcd8cb83
clem <clement.webm@gmail.com>
2023-12-11 20:08:33 +0100

    views

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import io from 'socket.io-client';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
+
 // Connectez-vous au serveur Socket.IO
 const socket = io('http://localhost:8888');
 
@@ -40,9 +49,47 @@ function LoginForm() {
   );
 }
 
-function MyApp() {
+const MyGame = ({jeu})=>{
   return (
+    <div style={{
+      backgroundColor:"blue",
+      // height:"300px",
+      width: "250px",
+      margin :"10px",
+      textAlign : "center",
+      padding: "250px 0"
+    }}>
+      <p>{jeu}</p>
+    </div>
+  )
+
+}
+
+
+function MyApp() {
+  
+
+  return (
+<<<<<<< HEAD
         <LoginForm />
+=======
+    <div className="container">
+        <Router>
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        component={LoginForm}
+                    />
+                    <Route
+                        exact
+                        path="/games"
+                        component={MyGame}
+                    />
+                </Routes>
+            </Router>
+    </div>
+>>>>>>> 52e2c70 (views)
   );
 }
 
--- test-app/src/index.js
+++ test-app/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { useState } from 'react';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
