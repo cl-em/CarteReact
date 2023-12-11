d09198bb48ca008431209faaea6a925d98d45d73
kyky <kylian.rigaud123@gmail.com>
2023-12-11 21:49:52 +0100

    interface

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -23,22 +23,56 @@ function test(){
 // let but = document.getElementById("but");
 // but.addEventListener("click",test);
 
+function RegisterForm() {
+  // Permet de naviger
+  const navigate = useNavigate();
+
+  return (
+    <div className="register-form">
+
+      <h2>Inscription</h2> <br></br>
+
+      <input type = "text" placeholder="Nom d'utilisateur" id="usernameRegister"></input> <br></br> <br></br>
+
+      <input type = "password" placeholder="Mot de passe" id="passwordRegister"></input> <br></br> <br></br>
+
+      <input type = "password" placeholder="Confirmer le mot de passe" id="passwordRegister2"></input> <br></br> <br></br> <br></br>
+
+      <button>S'inscrire !</button>
+      <button onClick={()=>navigate("/")}>Retourner à l'écran de connexion</button>
+      
+    </div>
+  );
+}
+
 function LoginForm() {
   // Permet de naviger
   const navigate = useNavigate();
 
   return (
     <div className="login-form">
-      <h2>Login</h2>
-      <input type = "text" placeholder="username" id="username"></input><br></br>
-      <input type = "password" placeholder="password" id="password"></input>
+      <h2>Connexion</h2>
+
       <br></br>
 
-      // Permet d'aller dans localhost/games
-      <button onClick={()=>navigate("/games")}>Connect!</button>
+      <input type = "text" placeholder="Nom d'utilisateur" id="usernameLogin"></input><br></br>
+
+      <br></br>
+
+      <input type = "password" placeholder="Mot de passe" id="passwordLogin"></input>
+
+      <br></br>
+      <br></br>
+
+      <button onClick={()=>navigate("/games")}>Envoyer!</button>
+
+      <p onClick={()=>navigate("/register")}>Vous n'avez pas de compte? Créez en un ici.</p>
+
     </div>
   );
 }
+
+//Permet d'aller dans localhost/games (up)
 
 const MyGame = ({jeu})=>{
   const navigate = useNavigate();
@@ -58,15 +92,14 @@ const MyGame = ({jeu})=>{
 
 }
 
-
+//Defini toutes tes pages ici
 function MyApp() {
   return (
     <div>
-      // Defini toutes tes pages ici
       <Router>
         <Routes>
           <Route path="/" element={<LoginForm />} />
-          <Route path="/register" element={} />
+          <Route path="/register" element={<RegisterForm />} />
           <Route path="/games" element={<MyGame jeu={"gros zizi"} />} />
         </Routes>
       </Router>
