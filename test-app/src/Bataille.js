6ae2f0d98059816937e8a5a1349af829b614840b
kyky <kylian.rigaud123@gmail.com>
2023-12-14 17:54:16 +0100

    d

--- test-app/src/Bataille.js
+++ test-app/src/Bataille.js
@@ -18,7 +18,33 @@ socket.emit("infoLobby",{idJoueur:"",idPartie:""});
 export function Lobby({listesjoueurs,nbjoueurs,joueursmax}){
     // listesjoueurs : liste de string,
 
-    return (<div><p>{nbjoueurs}/{joueursmax}</p></div>)
+    return (
+        
+    <div>
+        <div className='Table'>
+            <div className='j1'>
+            </div>
+            <div className='j2'>
+            </div>    
+            <div className='j3'>
+            </div>
+            <div className='j4'>
+            </div>
+            <div className='j5'>
+            </div>    
+            <div className='j6'>
+            </div>
+            <div className='j7'>
+            </div>
+            <div className='j8'>
+            </div>    
+            <div className='j9'>
+            </div>   
+            <div className='j10'>
+            </div> 
+        </div>
+    </div>
+    )
 }
 
 socket.on("infoLobby", (data) => { //liste de joueurs (liste de json), taille du paquet, liste de cartes, carte avec valeur et couleur comme attribut
