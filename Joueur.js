33b5cb0f9a3e958ff4207369a8cd1bfe2aaa83d9
Lukas Deronzier <lukasderonzier@gmail.com>
2024-03-24 10:38:11 +0100

    progess bar fix

--- Joueur.js
+++ Joueur.js
@@ -5,16 +5,17 @@ class Joueur{
 
     //La classe joueur définit l'état d'un joueur dans une partie donnée
 
-constructor(id,isHost){
+constructor(id,isHost,score){
     this.idJoueur = id; //Id du joueur, permettant de l'identifier lors d'une réception d'information par socket
     this.main = []; //Contenu de la main du joueur
     this.isHost = isHost; //Définit si le joueur est l'host de la partie ou non. Utile pour savoir si l'on peut lancer la partie    
     this.choix;
     this.éliminé = false;
+    this.score = score;
 }
 
 static fromJSON(data){
-    var joueur = new Joueur(data.idJoueur,data.isHost);
+    var joueur = new Joueur(data.idJoueur,data.isHost,data.score);
     joueur.main = data.main;
     return joueur;
 }
