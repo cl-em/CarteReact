bac09b3102821ba092daec0044f42dd831f7856c
clem <clement.webm@gmail.com>
2024-03-26 15:54:00 +0100

    maj choix avec un defaut

--- test-app/src/shadowhunter/ActionShadow.js
+++ test-app/src/shadowhunter/ActionShadow.js
@@ -66,7 +66,9 @@ export default function Action({ rapportAction, idJoueurLocal }) {
                         }}>{text}</button>
                     ))}
                 </div>
-            ) : <div></div>
+            ) : <div>
+                <img src={"http://localhost:8888/carteShadow"+actV.défaut+".png"} alt={actV.défaut}/>
+            </div>
             break
 
 
