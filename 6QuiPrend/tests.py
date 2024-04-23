12301086f30ebe74789da0934fa40317859b812a
Lukas Deronzier <lukasderonzier@gmail.com>
2024-04-23 13:56:38 +0200

    tests

--- 6QuiPrend/tests.py
+++ 6QuiPrend/tests.py
@@ -12,29 +12,21 @@ class Tests():
         self.game=NimmtGame(self.players)
         self.scores, self.winners=self.game.play()
     def save_game_stats(self, filename="game_stats.json"):
-        # stocker dans un json les infos suivantes: 
-        # le type de strategie, via la sous-classe ?
-        # le nombre de joueurs
-        # la nature des adversaires, via le nom des players ?
-        # le score des joueurs
-        # les gagnants
-        data = {
+        newdata = {
             "nb_bots":self.nb_bots,
-            "players":str(self.players),
-            "scores":str(self.scores),
-            "winners":str(self.winners)
+            "scores":self.scores,
+            "winners": [str(winner) for winner in self.winners]
         }
-        with open(filename, "w") as f:
-            f.write(json.dumps(data))
-
-    def generate_graphs(self):
-        # quels types de graphiques ?
-        # graphs des types de parties avec type de strat
-        # courbe de score différente pour chaque type de strat
-        # courbe de winner différente pour chaque type de strat
-        plt.plot(self.nb_bots)
-        plt.title("Nb Bots")
-        plt.savefig("nb_bots.png")
+        try:
+            with open(filename, 'r+') as file:
+                data = json.load(file)
+                data.append(newdata)
+                file.seek(0)
+                json.dump(data, file, indent=4)
+        except Exception:
+            with open(filename, 'w') as file:
+                json.dump([newdata], file, indent=4)
+                
 class TestsRandomFake(Tests):
     def __init__(self, nb_bots) -> None:
         super().__init__(nb_bots)
@@ -67,9 +59,112 @@ class BotMinVsMax(Tests):
         for i in range(nb_bots//2):
             self.players.append(BotMax(f"BotMax {i}"))
 
+class BotRandomFakeVsTrueRandom(Tests):
+    def __init__(self, nb_bots) -> None:
+        super().__init__(nb_bots)
+        for i in range(nb_bots//2):
+            self.players.append(BotRandomFake(f"BotRandomFake {i}"))
+        for i in range(nb_bots//2):
+            self.players.append(BotTrueRandom(f"BotTrueRandom {i}"))
+
+# def generate_graphs(battle_name):
+#         # quels types de graphiques ?
+#         # graphs des types de parties avec type de strat
+#         # courbe de score différente pour chaque type de strat
+#         # courbe de winner différente pour chaque type de strat
+#         filename = battle_name+".json"
+#         with open(filename, 'r') as file:
+#             data = json.load(file)
+#             scores_min, scores_max, scores_random_fake, scores_true_random = [], [], [], []
+#             winner_min, winner_max, winner_random_fake, winner_true_random = 0, 0, 0, 0
+#             for game in data:
+#                 for score in game["scores"]:
+#                     if "BotMin" in score:
+#                         scores_min.append(game["scores"][score])
+#                     elif "BotMax" in score:
+#                         scores_max.append(game["scores"][score])
+#                     elif "BotRandomFake" in score:
+#                         scores_random_fake.append(game["scores"][score])
+#                     elif "BotTrueRandom" in score:
+#                         scores_true_random.append(game["scores"][score])
+#                 for winner in game["winners"]:
+#                     if "BotMin" in winner:
+#                         winner_min+=1
+#                     elif "BotMax" in winner:
+#                         winner_max+=1
+#                     elif "BotRandomFake" in winner:
+#                         winner_random_fake+=1
+#                     elif "BotTrueRandom" in winner:
+#                         winner_true_random+=1
+#             if len(scores_min) > 0:
+#                 plt.plot(scores_min, label="BotMin")
+#             if len(scores_max) > 0:
+#                 plt.plot(scores_max, label="BotMax")
+#             if len(scores_random_fake) > 0:
+#                 plt.plot(scores_random_fake, label="BotRandomFake")
+#             if len(scores_true_random) > 0:
+#                 plt.plot(scores_true_random, label="BotTrueRandom")
+#             plt.legend()
+#             plt.legend()
+#             plt.savefig(battle_name+"_scores.png")
+#             # new plot for winners
+#             plt.clf()
+#             if winner_random_fake > 0:
+#                 plt.bar(["BotRandomFake", "BotTrueRandom"], [winner_random_fake, winner_true_random])
+#             elif winner_min > 0:
+#                 plt.bar(["BotMin", "BotMax"], [winner_min, winner_max])
+#             # plt.bar(["BotMin", "BotMax"], [winner_min, winner_max])
+#             plt.savefig(battle_name+"_winners.png")
+#             print("Graph generated")
+
+
+def stats(filename="game_stats.json"):
+    # ajouter nb joueurs
+    with open(filename, 'r') as file:
+        strats = []
+        data = json.load(file)
+        scores0 = data[0]["scores"]
+        for botname in scores0:
+            sous_chaine = botname[:-2]
+            if sous_chaine not in strats:
+                strats.append(sous_chaine)
+        scores = {}
+        winners = {}
+        for strat in strats:
+            scores[strat] = []
+            winners[strat] = 0
+        for game in data:
+            for botname in game["scores"]:
+                sous_chaine = botname[:-2]
+                scores[sous_chaine].append(game["scores"][botname])
+            for winner in game["winners"]:
+                sous_chaine = winner[:-2]
+                winners[sous_chaine] += 1
+    return scores, winners
+
+def generate_graphs(scores, winners):
+    for strat in scores:
+        print(strat)
+        plt.plot(scores[strat], label=strat)
+    plt.legend()
+    plt.title("Scores")
+    plt.savefig("_".join(scores.keys())+"_scores.png")
+    plt.clf()
+    plt.bar(list(winners.keys()), list(winners.values())) 
+    plt.title("Winners")
+    plt.savefig("_".join(winners.keys())+"_winners.png")
+    print("Graph generated")
+
 if __name__ == "__main__":
     for i in range(10):
-        testRandom = BotMinVsMax(nb_bots=10)
+        testRandom = BotRandomFakeVsTrueRandom(nb_bots=10)
         testRandom.start_game()
-        # testRandom.save_game_stats("BotMinVsMax.json")
-        testRandom.generate_graphs()
+        testRandom.save_game_stats("BotRandomFakeVsTrueRandom.json")
+    scores, winners = stats("BotRandomFakeVsTrueRandom.json")
+    generate_graphs(scores, winners)
+    for i in range(100):
+        testMinMax = BotMinVsMax(nb_bots=10)
+        testMinMax.start_game()
+        testMinMax.save_game_stats("BotMinMax.json")
+    scores, winners = stats("BotMinMax.json")
+    generate_graphs(scores, winners)
